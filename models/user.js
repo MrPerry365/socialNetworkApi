dd4fe6209f6fd38572bf5574b8744b54eaefe798
MrPerry365 <1987nowhere@gmail.com>
2022-07-24 20:09:59 -0400

    work on setup for user model

---
+++ models/user.js
@@ -0,0 +1,49 @@
+const { Schema, model, SchemaTypeOptions } = require("mongoose");
+
+// a new Schema for the user models in the database //
+userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+    },
+
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+// virtual that retrieves the length of the user's friends array field //
+userSchema.virtual("friendsCount").get(function () {
+  return this.friends.length;
+});
+
+
+const User = model("user", userSchema);
+module.exports = User;
